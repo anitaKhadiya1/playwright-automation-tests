d8979a424664fd05df5bee584623a28f307a6425
Anita <anitagadhavi64@gmail.com>
2026-03-26 17:30:38 +0530

    fix: set headless to true for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://opencart.antropy.co.uk/',
-     headless: false,
+     headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
